4853b8c95f194b631bffb4bf15bb0ab34a4e8ba3
Yann Lugrin <yann.lugrin@antistatique.net>
2023-02-10 16:02:07 +0100

    Setup eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-function App() {
-  return (
-    <h1 className="text-3xl font-bold underline text-center">Windmill</h1>
-  )
-}
+const App = () => (
+  <div className="App pt-4">
+    <h1 className="mb-4 text-center text-3xl font-bold underline">Windmill</h1>
+  </div>
+);
 
-export default App
+export default App;
